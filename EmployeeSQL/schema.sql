caa7db6b9a446eefca28216415140acd4c188676
Nida Ballinger-Chaudhary <147389952+NidaB-C@users.noreply.github.com>
2023-12-13 10:39:58 +0000

    Update to schema

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,14 +1,14 @@
 -- Create Departments Table 
 CREATE TABLE Departments (
     dept_no VARCHAR(4) NOT NULL,
-    dept_name VARCHAR(18) NOT NULL,
+    dept_name VARCHAR(25) NOT NULL,
     PRIMARY KEY (dept_no)
 );
 
 -- Create Titles Table 
 CREATE TABLE Titles (
     title_id VARCHAR(5) NOT NULL,
-    title VARCHAR(18) NOT NULL,
+    title VARCHAR(30) NOT NULL,
     PRIMARY KEY (title_id)
 );
 
@@ -17,8 +17,8 @@ CREATE TABLE Employees (
     emp_no INT NOT NULL,
     emp_title_id VARCHAR(5) NOT NULL,
     birth_date DATE NOT NULL,
-    first_name VARCHAR(14) NOT NULL,
-    last_name VARCHAR(16) NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     sex CHAR(1) NOT NULL,
     hire_date DATE NOT NULL,
     PRIMARY KEY (emp_no),
